489f338b5d5082c984c2144f7765ed78292b203b
Aman Verma <amanv9952@gmail.com>
2026-03-18 23:01:44 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ import serviceAppointmentRouter from "./routes/serviceAppointmentRouter.js";
 const app = express();
 const port = 4000;
 
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  process.env.ADMIN_URL,
+].filter(Boolean);
 
 // Middleware
 app.use(
